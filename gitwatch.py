415e6ef04f2a7bb063a3ac1bfb52f2884e7852d8
Jon Godshall <jon.godshall@gmail.com>
2017-11-27 11:39:29 -0600

    Add count of merges, broken down by week

--- gitwatch.py
+++ gitwatch.py
@@ -2,7 +2,8 @@ import sys
 import os
 import json
 import http.client
-from datetime import datetime
+from datetime import datetime, timedelta
+
 from urllib.parse import urlparse, parse_qs
 
 #constants for api calls
@@ -36,7 +37,7 @@ class PullRequest:
 
     def __str__(self):
         merge_text = 'Merged' if self.merged else 'Not Merged'
-        return '%s - %s at %s (%s)\n' % (str(self.id), self.user.name, str(self.created_at), merge_text)
+        return '%s - %s at %s (%s) at %s\n' % (str(self.id), self.user.name, str(self.created_at), merge_text, str(self.merged_at))
 
 class User:
     def __init__(self, id, username, is_site_admin):
@@ -67,6 +68,7 @@ class Repo:
             pulls = json.loads(json_string)
         except:
             print('Unable to parse json string: %s' % json_string)
+            return
 
         #Miniimum dataset should be who requested it, location of the new repo, when was it requested, has it been merged and by who
         for obj in pulls:
@@ -75,13 +77,14 @@ class Repo:
 
             pull_request.user = User(obj['user']['id'], obj['user']['login'], obj['user']['site_admin'])
             pull_request.merged = False if obj['merged_at'] == None else True
+            pull_request.merged_at = None if obj['merged_at'] == None else datetime.strptime(obj['merged_at'], '%Y-%m-%dT%H:%M:%SZ')
 
             #This is likely to be useful info, but it chews up a lot of requests and they are rate limited.  Use only if needed
             if load_details and pull_request.merged:
                 merge_details = get_json_content('/repos/%s/%s/pulls/%s' % (self.owner, self.name, obj['number']))
                 try:
                     pull = json.loads(merge_details)
-                    pull_request.merged_at = datetime.strptime(pull['merged_at'],'%Y-%m-%dT%H:%M:%SZ')
+                    #pull_request.merged_at = datetime.strptime(pull['merged_at'],'%Y-%m-%dT%H:%M:%SZ')
                     pull_request.merged_by = User(pull['merged_by']['id'], pull['merged_by']['login'], pull['merged_by']['site_admin'])
                 except:
                     print('Could not load merge details: /repos/%s/%s/pulls/%s' % (self.owner, self.name, obj['number']))
@@ -106,6 +109,28 @@ def get_org_repos(org):
 
     return [Repo(obj['id'], obj['name'], org) for obj in response_repos]
 
+#Build the list of all pulls, regardless of repo
+def combine_repos(repos):
+    ret = []
+    for r in repos:
+        try:
+            r.extend(r.pull_requests)
+        except:
+            print('No pull requests for: %s' % r.name)
+
+    return ret
+
+def get_total_pulls(repos):
+    ret = {}
+    for r in repos:
+        for p in r.pull_requests:
+            if not p.merged_at == None:
+                if not p.merged_at.date() in ret:
+                    ret[p.merged_at.date()] = 0
+                ret[p.merged_at.date()] += 1
+    return ret
+
+
 #main block
 def main():
     try:
@@ -120,11 +145,45 @@ def main():
         os.makedirs('tmp')
 
     for repo in repos:
-        f = open('tmp/%s-%s.txt' % (org, repo.name), 'w')
         repo.refresh_pull_requests()
-        print('%s - %d' % (repo.name, len(repo.pull_requests)))
-        for p in repo.pull_requests:
-            f.write(str(p))
+    #    f = open('tmp/%s-%s.txt' % (org, repo.name), 'w')
+
+    #    print('%s - %d' % (repo.name, len(repo.pull_requests)))
+    #    for p in repo.pull_requests:
+    #        f.write(str(p))
+
+    merges_by_day = get_total_pulls(repos)
+    #import ipdb; ipdb.set_trace()
+
+    #week starts on Friday (4)
+    sorted_keys = sorted(merges_by_day)
+    ret = {}
+
+    start = sorted_keys[0]
+    end = sorted_keys[len(sorted_keys)-1]
+    d = start
+    start_day = start.weekday();
+
+    week = str(start - timedelta(days=(4 - start_day)))
+    ret[week] = 0
+
+
+    while not d > end:
+        day = d.weekday()
+
+        if day == 4:
+            #start new week
+            week = str(d)
+            ret[week] = 0
+
+        if d in merges_by_day:
+            ret[week] += merges_by_day[d]
+
+
+        d = d + timedelta(days=1)
+
+    for week in ret:
+        print('%s - %d' % (week, ret[week]))
 
     conn.close()
 
